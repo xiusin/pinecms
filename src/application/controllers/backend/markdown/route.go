e11b29bc5ede2cab549d615d1a4465d4f64f01ef
xiusin <826466266@qq.com>
2021-11-06 09:49:16 +0800

    添加doc生成功能

---
+++ src/application/controllers/backend/markdown/route.go
@@ -0,0 +1,18 @@
+package wechat
+
+import (
+	"github.com/xiusin/pine"
+)
+
+func InitRouter(app *pine.Application, router *pine.Router) {
+	app.ANY("/api/wechat/msg/:appid", msgHandler)
+	router.Handle(new(WechatAccountController), "/wechat/account")
+	router.Handle(new(WechatUserController), "/wechat/user")
+	router.Handle(new(WechatMagController), "/wechat/msg")
+	router.Handle(new(WechatQrcodeController), "/wechat/qrcode")
+	router.Handle(new(WechatRuleController), "/wechat/rule")
+	router.Handle(new(WechatMaterialController), "/wechat/material")
+	router.Handle(new(WechatMsgTemplateController), "/wechat/template")
+	router.Handle(new(WechatUserTagsController), "/wechat/user/tags")
+	router.Handle(new(WechatMenuController), "/wechat/menu")
+}
