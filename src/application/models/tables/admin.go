93b5d777c2114353f22b2b72b9dead08d66807ba
xiusin <chenchengbin92@gmail.com>
2022-03-05 16:59:50 +0800

    新增一些字段

--- src/application/models/tables/admin.go
+++ src/application/models/tables/admin.go
@@ -1,23 +1,24 @@
 package tables
 
 type Admin struct {
-	Userid        int64  `xorm:"pk autoincr 'id'" json:"id"`
-	Username      string `json:"username"`
-	Password      string `json:"-"`
-	Roleid        int64  `json:"roleid" xorm:"-"`
-	Encrypt       string `json:"-"`
-	Lastloginip   string `json:"lastloginip"`
-	Lastlogintime int64  `json:"lastlogintime"`
-	Email         string `json:"email"`
-	Realname      string `json:"realname"`
-	Avatar        string `json:"avatar"`
-	Remark        string `json:"remark"`
-	RoleName      string `json:"roleName" xorm:"-"`
-	Phone         string `json:"phone"`
-	Status        uint   `json:"status"`
-	PositionId    uint   `json:"position_id"`
-	LevelId       uint   `json:"level_id"`
-	DepartmentId  uint   `json:"department_id"`
+	Userid        int64      `xorm:"pk autoincr 'id'" json:"id"`
+	Username      string     `json:"username"`
+	Password      string     `json:"-"`
+	Roleid        int64      `json:"roleid" xorm:"-"`
+	Encrypt       string     `json:"-"`
+	Lastloginip   string     `json:"lastloginip"`
+	Lastlogintime int64      `json:"lastlogintime"`
+	Email         string     `json:"email"`
+	Realname      string     `json:"realname"`
+	Avatar        string     `json:"avatar"`
+	Remark        string     `json:"remark"`
+	RoleName      string     `json:"roleName" xorm:"-"`
+	Phone         string     `json:"phone"`
+	Status        uint       `json:"status"`
+	PositionId    uint       `json:"position_id"`
+	LevelId       uint       `json:"level_id"`
+	DepartmentId  uint       `json:"department_id"`
+	Birthday      *LocalTime `json:"birthday"`
 
 	RoleIdList []int64 `json:"roleIdList" xorm:"json roles"`
 }
